deba69b892c70814f79896e3458b2eb1589a0336
Allis21 <allishincapie21@gmail.com>
2025-02-23 17:26:24 -0500

    Program resource

---
+++ src/main/java/co/edu/uniquindio/apis/dtos/ProgramResponseDTO.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.apis.dtos;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import co.edu.uniquindio.apis.model.Comment;
+
+public class ProgramResponseDTO {
+    String id;
+    String title;
+    String description;
+    String content;
+    String creatorId;
+    List<Comment> comments;
+    LocalDateTime creationDate;
+}
